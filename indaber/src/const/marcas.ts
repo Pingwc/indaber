9695733484543e596781056b942bc6e883e102b6
Pingwc <pingwanch@gmail.com>
2024-04-08 11:41:39 +0200

    Add marcas section

---
+++ indaber/src/const/marcas.ts
@@ -0,0 +1,32 @@
+export const MARCAS = [
+	{
+		id: "solaredge",
+		name: "Solar Edge",
+		url: "https://www.solaredge.com/es/",
+	},
+	{
+		id: "victron",
+		name: "Victron",
+		url: "https://www.victronenergy.com.es/",
+	},
+	{
+		id: "goodwe",
+		name: "Goodwe",
+		url: "https://es.goodwe.com/",
+	},
+	{
+		id: "SMA",
+		name: "Sunny Portal",
+		url: "https://www.sma.de/es/",
+	},
+	{
+		id: "salicru",
+		name: "Salicru",
+		url: "https://www.salicru.com/",
+	},
+	{
+		id: "ingeteam",
+		name: "Ingeconsun",
+		url: "https://www.ingeteam.com/es-es/inicio.aspx",
+	},
+] as const
