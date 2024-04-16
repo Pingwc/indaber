3f4b2716d72b660b8b8ac7950408d4ff8384e450
Pingwc <pingwanch@gmail.com>
2024-04-16 13:27:09 +0200

    Add clientes route

---
+++ indaber/src/const/clientes.ts
@@ -0,0 +1,23 @@
+export const CLIENTES = [
+	{
+		id: "escudo_SanAntonio",
+		name: "Ayuntamiento de San Antonio",
+		url: "https://www.santantoni.net/es",
+	},
+	{
+		id: "escudo_SantaEulalia",
+		name: "Ayuntamiento de Santa Eulalia",
+		url: "https://santaeulariadesriu.com/es/",
+	},
+	{
+		id: "escudo_Formentera",
+		name: "Consell Insular de Formentera",
+		url: "https://www.consellinsulardeformentera.cat/index.php?lang=es",
+	},
+	{
+		id: "ferrovial",
+		name: "Ferrovial",
+		url: "https://www.ferrovial.com/es/",
+	},
+
+] as const
