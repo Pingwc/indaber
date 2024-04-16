ac9d50a7e5f9da2d44bf11727a50c54c35b28ec0
Pingwc <pingwanch@gmail.com>
2024-04-16 13:49:21 +0200

    Add netlify adapter

--- indaber/astro.config.mjs
+++ indaber/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import netlify from "@astrojs/netlify";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  adapter:netlify(),
+  output: 'server'
 });
