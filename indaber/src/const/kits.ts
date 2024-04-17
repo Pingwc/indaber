0c0caa4d459309d2e74d1c8cc0126dd6de48ea82
Pingwc <pingwanch@gmail.com>
2024-04-17 15:45:37 +0200

    Add dynamic routes for kits from file

---
+++ indaber/src/const/kits.ts
@@ -0,0 +1,39 @@
+export const KITS = [
+	{
+		id: "kit-solar-autoconsumo-directo-4kw-monocristalino-huawei-sun2000-5ktl",
+        num: 1,
+		nombre: "Kit Solar Autoconsumo Directo 4kW Monocristalino HUAWEI SUN2000 5KTL",
+		precio: 3550,
+        descripcion: `Kit solar de autoconsumo directo con 4 kwp de paneles e inversor de conexión a red  HUAWEI SUN2000 5KTL.
+        Es adecuado para instalaciones en las que se dispone de una potencia contratada de 4kW de conexión a red.`,
+        imagen:'/img/kits/kit_1.jpg'
+	},
+	{
+		id: "kit-solar-autoconsumo-directo-4kw-monocristalino-huawei-sun2000-3ktl",
+        num: 2, 
+		nombre: "Kit Solar Autoconsumo Directo 4kW Monocristalino HUAWEI SUN2000 5KTL",
+		precio: 2000,
+        descripcion: `Kit solar de autoconsumo directo con 1 kw de paneles e inversor de conexión a red  HUAWEI SUN2000 3KTL.
+        Es adecuado para instalaciones en las que se dispone de una potencia contratada de 1kW de conexión a red.
+        Gracias a su diseño y simplicidad permite una sencilla puesta en marcha y prácticamente no precisa mantenimiento permitiendo que su amortización se produzca más rápidamente`,
+        imagen:'/img/kits/kit_2.jpg',
+	},
+	{
+		id: "kit-solar-autoconsumo-directo-3kwp–solis-s5-eh1P-3.6k-y-tres-baterias-pylontech",
+        num: 3,
+		nombre: "Kit Solar Autoconsumo Directo 3kWp – SOLIS-S5 EH1P-3.6K y tres baterías Pylontech",
+		precio: 7000,
+        descripcion: `Kit solar de autoconsumo directo con 3 kwp de paneles, inversor híbrido SOLIS S5-EH1P3.6K-L y tres baterías de litio Pylontech.`,
+        imagen:'/img/kits/kit_3.jpg'
+	},
+	{
+		id: "kit-solar-autoconsumo-directo-5.02kwp–kostal-5.0-2-mp-plus-y-una-bateria-de-litio-byd-10.2",
+        num: 4,
+		nombre: "Kit Solar Autoconsumo Directo 5.02kWp – Kostal 5.0-2 MP plus y una batería de litio BYD 10.2",
+		precio: 13000,
+        descripcion: `Kit solar de autoconsumo directo con 5,02 kwp de paneles, inversor de conexión a red monofásico PIKO MP plus 5.0-2 Kostal,
+        y batería de litio BYD 10.2. Este inversor es adecuado para instalaciones en las que se dispone de una potencia contratada de 5 kw de conexión a red. `,
+        imagen:'/img/kits/kit_4.jpg'
+	},
+
+] as const
